f31a0f49bd7fb3e786d90b612123eadd18ed3cfd
ogustavoress <ogustavoress@gmail.com>
2025-05-22 19:29:10 -0300

    Incluindo Toolbar ao projeto

--- app/src/main/java/ogustavoress/com/github/androidlistadecompras/MainActivity.kt
+++ app/src/main/java/ogustavoress/com/github/androidlistadecompras/MainActivity.kt
@@ -1,9 +1,12 @@
 package ogustavoress.com.github.androidlistadecompras
 
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,11 +16,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ogustavoress.com.github.androidlistadecompras.ui.theme.AndroidListaDeComprasTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Lista de Compras"
     }
 }
