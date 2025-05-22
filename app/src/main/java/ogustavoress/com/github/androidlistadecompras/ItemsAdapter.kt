0aed7348d242978e0a48335b8b2d8b439072b371
ogustavoress <ogustavoress@gmail.com>
2025-05-22 19:40:43 -0300

    Criando ItemsAdapter e implementando membros de RecyclerView

---
+++ app/src/main/java/ogustavoress/com/github/androidlistadecompras/ItemsAdapter.kt
@@ -0,0 +1,24 @@
+package ogustavoress.com.github.androidlistadecompras
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemsAdapter : RecyclerView.Adapter<ItemsAdapter.ItemViewHolder>(){
+
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
