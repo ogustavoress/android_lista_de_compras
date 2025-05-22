b15b7617f1ef091b03c14d928ce5cb62ee315548
ogustavoress <ogustavoress@gmail.com>
2025-05-22 17:43:42 -0300

    Implementando a dependência RecyclerView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
 }
