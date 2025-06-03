56ed167daeb2db7f5790cc413164ca349ef3b0a8
ogustavoress <ogustavoress@gmail.com>
2025-06-02 22:27:57 -0300

    Criando uma lista ficticia, fazendo a chamada da RecyclerView, definindo o layout da lista e conectando o ItemAdapter

--- app/src/main/java/ogustavoress/com/github/androidlistadecompras/MainActivity.kt
+++ app/src/main/java/ogustavoress/com/github/androidlistadecompras/MainActivity.kt
@@ -1,21 +1,32 @@
 package ogustavoress.com.github.androidlistadecompras
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ogustavoress.com.github.androidlistadecompras.ui.theme.AndroidListaDeComprasTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    //Lista de Teste para preencher a RecyclerView
+    private val listaDeCompras = listOf(
+        ItemCompra("Arroz", 2),
+        ItemCompra("Feijão", 1),
+        ItemCompra("Macarrão", 3),
+        ItemCompra("Tomate", 1)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+        setContentView(R.layout.activity_main)
+
+        //1. Referência para o RecyclerView do XML
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewItens)
+
+        //2. Define o layout da lista (vertical)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        //3. Conecta o RecyclerView com o adapter, passando a lista de itens
+        recyclerView.adapter = ItemAdapter(listaDeCompras)
+
     }
 }
