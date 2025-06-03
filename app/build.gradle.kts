cfde29ce759098e02198ff4175d483c5fdb4a770
ogustavoress <ogustavoress@gmail.com>
2025-06-02 23:39:03 -0300

    Corrigindo falhas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -35,7 +34,7 @@ android {
         jvmTarget = "11"
     }
     buildFeatures {
-        compose = true
+        compose = false
     }
 }
 
