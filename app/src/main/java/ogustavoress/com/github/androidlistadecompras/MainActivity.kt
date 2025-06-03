7ad3fe1b14c16fc94777bf18093b370109b1150a
ogustavoress <ogustavoress@gmail.com>
2025-06-02 22:07:12 -0300

    Limpando o arquivo MainActivity.kt

--- app/src/main/java/ogustavoress/com/github/androidlistadecompras/MainActivity.kt
+++ app/src/main/java/ogustavoress/com/github/androidlistadecompras/MainActivity.kt
@@ -16,32 +16,6 @@ import ogustavoress.com.github.androidlistadecompras.ui.theme.AndroidListaDeComp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AndroidListaDeComprasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidListaDeComprasTheme {
-        Greeting("Android")
+        
     }
 }
